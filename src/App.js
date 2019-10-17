b354fb4d4bc9833b084035171da0247c18eab8bc
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-17 13:47:04 -0700

    Add initial state/component setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  // This is alternative class component syntax - class field declaration. See https://github.com/the-road-to-learn-react/react-alternative-class-component-syntax and https://www.robinwieruch.de/react-state-without-constructor
+  state = {
+    todos: [
+      {id: 1, content: 'Buy milk'},
+      {id: 2, content: 'Call mom'}
+    ]
+  }
 
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
 }
 
 export default App;
