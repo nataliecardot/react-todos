ca1619fa79d81f265582ca58e61a4bea7db14797
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-20 18:19:06 -0700

    Change build script, build project

---
+++ docs/build/precache-manifest.343520ababa10c13e7f125fa582203a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d04405c89277168d4ede25de0262de",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "7566e50d1eed1dceb096",
+    "url": "/react-todos/static/css/main.33d3f64e.chunk.css"
+  },
+  {
+    "revision": "1d95346a567f044b7e2a",
+    "url": "/react-todos/static/js/2.50984575.chunk.js"
+  },
+  {
+    "revision": "7566e50d1eed1dceb096",
+    "url": "/react-todos/static/js/main.9e6f815a.chunk.js"
+  },
+  {
+    "revision": "c201740ec6801c26a7aa",
+    "url": "/react-todos/static/js/runtime-main.5792db05.js"
+  }
+]);
