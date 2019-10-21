521686fe141082dd448f44060f7d0948687e6582
Natalie-Cardot <nataliecardot@gmail.com>
2019-10-20 22:00:51 -0700

    Rebuild

--- docs/precache-manifest.19fbb652aabf1c9d6471feff2be6184b.js
+++ docs/precache-manifest.19fbb652aabf1c9d6471feff2be6184b.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b7ea6889a5e1289bceacc4ac075cbb6",
+    "revision": "1f4a566cd4b8710c46a2b692d2c44397",
     "url": "/react-todos/index.html"
   },
   {
-    "revision": "6e3920866ef79df3a943",
-    "url": "/react-todos/static/css/main.33d3f64e.chunk.css"
+    "revision": "1c64e1df0294c063d9f8",
+    "url": "/react-todos/static/css/main.ce531cc7.chunk.css"
   },
   {
     "revision": "1d95346a567f044b7e2a",
     "url": "/react-todos/static/js/2.50984575.chunk.js"
   },
   {
-    "revision": "6e3920866ef79df3a943",
-    "url": "/react-todos/static/js/main.8aa2145c.chunk.js"
+    "revision": "1c64e1df0294c063d9f8",
+    "url": "/react-todos/static/js/main.7e41b482.chunk.js"
   },
   {
     "revision": "c201740ec6801c26a7aa",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-todos/precache-manifest.3927a5ed123aab5107e85e5ffb5ec2c1.js"
+  "/react-todos/precache-manifest.19fbb652aabf1c9d6471feff2be6184b.js"
 );
 
 self.addEventListener('message', (event) => {
